5c398c4c38a10570eacd297f59e0aa85a481be0f
OwnNightmare <yura.borisov.9523@gmail.com>
2021-11-16 11:34:55 +0300

    Added genre table

--- scratch_2.sql
+++ scratch_2.sql
@@ -1,19 +1,26 @@
-create table if not exists performer(
-id serial primary key,
-performer_name varchar(30) not null
+CREATE TABLE if NOT EXISTS performer(
+id serial PRIMARY KEY,
+performer_name VARCHAR(30) NOT NULL
 );
 
-create table if not exists album(
-id serial primary key,
-album_name varchar(20) not null,
-release_year integer,
-performer_id integer references performer(id)
+CREATE TABLE if NOT EXISTS album(
+id serial PRIMARY KEY,
+album_name VARCHAR(40) NOT NULL,
+release_year DATE,
+performer_id INTEGER REFERENCES performer(id)
 );
 
-create table if not exists track(
-id serial primary key,
-track_name varchar(40) not null,
-duration interval,
-performer_id integer references performer(id),
-album_id integer references album(id)
+CREATE TABLE if NOT EXISTS track(
+id serial PRIMARY KEY,
+track_name VARCHAR(25) NOT NULL,
+duration INTEGER,
+album_id INTEGER REFERENCES album(id)
 );
+
+CREATE TABLE if NOT EXISTS genre(
+genre_name VARCHAR(20) PRIMARY KEY
+);
+
+ALTER TABLE performer
+ADD COLUMN genre VARCHAR(20) REFERENCES genre(genre_name)
+;
