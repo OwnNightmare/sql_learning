aae235dce6e3e1fd004d88e9dc6af6d6e04ae87b
OwnNightmare <yura.borisov.9523@gmail.com>
2021-11-23 09:39:04 +0300

    Changed for correct fourth task

--- create_alter.sql
+++ create_alter.sql
@@ -7,10 +7,16 @@ CONSTRAINT PK_Artist PRIMARY KEY(artist_id)
 CREATE TABLE if NOT EXISTS Album(
 album_id SERIAL,
 album_name VARCHAR(40) NOT NULL,
-artist_id INTEGER NOT NULL,
 release_year INTEGER,
-CONSTRAINT PK_Album PRIMARY KEY (album_id),
-FOREIGN KEY (artist_id) REFERENCES Artist(artist_id)
+CONSTRAINT PK_Album PRIMARY KEY (album_id)
+);
+
+CREATE TABLE if NOT EXISTS Artists_albums(
+artist_id INTEGER,
+album_id INTEGER,
+CONSTRAINT PK_artist_album PRIMARY KEY(artist_id, album_id),
+FOREIGN KEY (artist_id) REFERENCES Artist(artist_id),
+FOREIGN KEY (genre_id) REFERENCES Genre(genre_id)
 );
 
 CREATE TABLE if NOT EXISTS Genre(
@@ -19,24 +25,30 @@ genre_name VARCHAR(40) NOT NULL,
 CONSTRAINT PK_genre PRIMARY KEY(genre_id)
 );
 
+CREATE TABLE if NOT EXISTS Artists_genre(
+artist_id INTEGER,
+genre_id INTEGER,
+CONSTRAINT PK_artists_genre PRIMARY KEY(artist_id, genre_id)
+FOREIGN KEY (artist_id) REFERENCES Artist(artist_id),
+FOREIGN KEY (genre_id) REFERENCES Genre(genre_id)
+);
+
 CREATE TABLE if NOT EXISTS Track(
 track_id SERIAL,
 track_name VARCHAR(40) NOT NULL,
 duration INTEGER,
 album_id INTEGER,
-genre_id INTEGER,
 CONSTRAINT PK_Track PRIMARY KEY (track_id),
-FOREIGN KEY (album_id) REFERENCES Album (album_id),
-FOREIGN KEY (genre_id) REFERENCES Genre (genre_id)
+CONSTRAINT unique_track UNIQUE(track_name),
+FOREIGN KEY (album_id) REFERENCES Album (album_id)
 );
 
-ALTER TABLE track
-ADD CONSTRAINT unique_track UNIQUE(track_name);
 
 CREATE TABLE if NOT EXISTS Mixtape(
 mixtape_id SERIAL,
 mixtape_name VARCHAR(30) NOT NULL,
-CONSTRAINT PK_mixtape PRIMARY KEY(mixtape_id)
+CONSTRAINT PK_mixtape PRIMARY KEY(mixtape_id),
+CONSTRAINT unique_mix UNIQUE(mixtape_name)
 );
 
  CREATE TABLE if NOT EXISTS TrackMixtape(
@@ -48,48 +60,16 @@ CONSTRAINT PK_mixtape PRIMARY KEY(mixtape_id)
  );
 
 ALTER TABLE Mixtape
-ADD CONSTRAINT unique_mix UNIQUE(mixtape_name);
-
-ALTER TABLE Mixtape
 ADD COLUMN release_year INTEGER;
 
 ALTER TABLE Artist
 ADD CONSTRAINT unique_artist UNIQUE(artist_name);
 
-INSERT INTO Artist(artist_name)
-VALUES('Oxxxymiron')
-ON CONFLICT DO NOTHING;
+ALTER TABLE Genre
+ADD CONSTRAINT unique_genre UNIQUE(genre_name);
 
-INSERT INTO Track(track_name, duration)
-VALUES('Moh', 175)
-ON CONFLICT DO NOTHING;
+ALTER TABLE Album
+ADD CONSTRAINT unique_album UNIQUE(album_name);
 
-INSERT INTO Genre(genre_name)
-VALUES('Hip-hop')
-ON CONFLICT DO NOTHING;
 
-INSERT INTO Album
-VALUES(1, 'Dark Time', 1, 2021)
-ON CONFLICT DO NOTHING;
 
-UPDATE Track
-    SET album_id = 1, genre_id = 2
-    WHERE track_id = 8;
-
- INSERT INTO Mixtape(mixtape_name)
- VALUES('Moh is all around us')
- ON CONFLICT DO NOTHING;
-
- UPDATE Mixtapes
-    SET release_year = 2020
-    WHERE mixtape_id = 2;
-
- INSERT INTO TrackMixtape
- VALUES(8,2)
- ON CONFLICT DO NOTHING;
-
- ALTER TABLE Genre
- ADD CONSTRAINT unique_genre UNIQUE(genre_name);
-
- ALTER TABLE Album
- ADD CONSTRAINT unique_album UNIQUE(album_name);
--- selecting_module.py
+++ selecting_module.py
@@ -1,94 +1,96 @@
-import sqlalchemy
-from pprint import pprint
+if __name__ == '__main':
 
-db = 'postgresql://me:password@localhost:5432/newdb'
-engine = sqlalchemy.create_engine(db)
-connection = engine.connect()
+    import sqlalchemy
+    from pprint import pprint
 
-
-def get_albums():
-    albums2018 = connection.execute("""SELECT album_name AS album, release_year AS release
-    FROM Album
-    WHERE release_year = 2018
-    ORDER BY release_year DESC
-    """).fetchall()
-    pprint(albums2018)
+    db = 'postgresql://me:password@localhost:5432/newdb'
+    engine = sqlalchemy.create_engine(db)
+    connection = engine.connect()
 
 
-def get_longest():
-    longest = connection.execute("""SELECT track_name, duration 
-    FROM Track
-    ORDER BY duration DESC
-    LIMIT 1""").fetchall()
-    pprint(longest)
+    def get_albums():
+        albums2018 = connection.execute("""SELECT album_name AS album, release_year AS release
+        FROM Album
+        WHERE release_year = 2018
+        ORDER BY release_year DESC
+        """).fetchall()
+        pprint(albums2018)
 
 
-def get_long_tracks():
-    long_tracks = connection.execute(""" SELECT track_name
-    FROM Track
-    WHERE duration > 210 """).fetchall()
-    pprint(long_tracks)
+    def get_longest():
+        longest = connection.execute("""SELECT track_name, duration 
+        FROM Track
+        ORDER BY duration DESC
+        LIMIT 1""").fetchall()
+        pprint(longest)
 
 
-def get_one_worded():
-    one_worded = connection.execute("""SELECT artist_name
-    FROM Artist
-    WHERE TRIM (TRAILING ' ' FROM artist_name) NOT LIKE '%% %%' 
-    """).fetchall()
-    pprint(one_worded)
+    def get_long_tracks():
+        long_tracks = connection.execute(""" SELECT track_name
+        FROM Track
+        WHERE duration > 210 """).fetchall()
+        pprint(long_tracks)
 
 
-def get_with_my():
-    includes_my = connection.execute("""SELECT track_name
-    FROM Track
-    WHERE track_name iLIKE '%%my%%' 
-    or track_name iLIKE '%%мой%%' """).fetchall()
-    pprint(includes_my)
+    def get_one_worded():
+        one_worded = connection.execute("""SELECT artist_name
+        FROM Artist
+        WHERE TRIM (TRAILING ' ' FROM artist_name) NOT LIKE '%% %%' 
+        """).fetchall()
+        pprint(one_worded)
 
 
-def get_mix_in_18_20():
-    between18_20 = connection.execute("""SELECT mixtape_name 
-    FROM Mixtape
-    WHERE release_year BETWEEN 2018 and 2020""").fetchall()
-    pprint(between18_20)
+    def get_with_my():
+        includes_my = connection.execute("""SELECT track_name
+        FROM Track
+        WHERE track_name iLIKE '%%my%%' 
+        or track_name iLIKE '%%мой%%' """).fetchall()
+        pprint(includes_my)
 
 
-def run_display():
-    query = ''
-    help = (""" 
-    'al' - название и год выхода альбомов, вышедших в 2018 году;
-    'lt' - название и продолжительность самого длительного трека;
-    'lts' - название треков, продолжительность которых не менее 3,5 минуты;
-    'mx' - названия сборников, вышедших в период с 2018 по 2020 год включительно;
-    'art' - исполнители, чье имя состоит из 1 слова;
-    'my' - название треков, которые содержат слово "мой"/"my" 
-    """)
-    print(help)
-    while query not in ['q', 'exit']:
-        query = input('\tЗапрос: ').lower()
-        if query == 'al':
-            print('Альбомы, вышедшие в 2018:')
-            get_albums()
-        elif query == 'lt':
-            print('Самый продолжительный трек: ')
-            get_longest()
-        elif query == 'lts':
-            print('Треки не менее 3.5 минут: ')
-            get_long_tracks()
-        elif query == 'mx':
-            print('Сборники 2018 - 2020: ')
-            get_mix_in_18_20()
-        elif query == 'art':
-            print('Имена артистов из 1 слова: ')
-            get_one_worded()
-        elif query == 'my':
-            print('Трек содержит слово my/мой')
-            get_with_my()
-        elif query == 'h':
-            print('\tСправка')
-            print(help)
-        else:
-            print('Unknown query. h - help' )
+    def get_mix_in_18_20():
+        between18_20 = connection.execute("""SELECT mixtape_name 
+        FROM Mixtape
+        WHERE release_year BETWEEN 2018 and 2020""").fetchall()
+        pprint(between18_20)
 
 
-run_display()
+    def run_display():
+        query = ''
+        helping = (""" 
+        'al' - название и год выхода альбомов, вышедших в 2018 году;
+        'lt' - название и продолжительность самого длительного трека;
+        'lts' - название треков, продолжительность которых не менее 3,5 минуты;
+        'mx' - названия сборников, вышедших в период с 2018 по 2020 год включительно;
+        'art' - исполнители, чье имя состоит из 1 слова;
+        'my' - название треков, которые содержат слово "мой"/"my" 
+        """)
+        print(helping)
+        while query not in ['q', 'exit']:
+            query = input('\tЗапрос: ').lower()
+            if query == 'al':
+                print('Альбомы, вышедшие в 2018:')
+                get_albums()
+            elif query == 'lt':
+                print('Самый продолжительный трек: ')
+                get_longest()
+            elif query == 'lts':
+                print('Треки не менее 3.5 минут: ')
+                get_long_tracks()
+            elif query == 'mx':
+                print('Сборники 2018 - 2020: ')
+                get_mix_in_18_20()
+            elif query == 'art':
+                print('Имена артистов из 1 слова: ')
+                get_one_worded()
+            elif query == 'my':
+                print('Трек содержит слово my/мой')
+                get_with_my()
+            elif query == 'h':
+                print('\tСправка')
+                print(help)
+            else:
+                print('Unknown query. h - help')
+
+
+    run_display()
