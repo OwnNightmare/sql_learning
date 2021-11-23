94ef59da3220d090fb7f899ba0b8387752f38adc
OwnNightmare <yura.borisov.9523@gmail.com>
2021-11-23 16:06:11 +0300

    using SQLAlchemy Metadata

--- FourthTask/inserting_module.py
+++ FourthTask/inserting_module.py
@@ -1,19 +1,21 @@
 import sqlalchemy
+from sqlalchemy import *
+metadata = MetaData()
 
-db = 'postgresql://me:password@localhost:5432/newdb'
+db = 'postgresql://me:password@localhost:5432/my_db'
 engine = sqlalchemy.create_engine(db)
 connection = engine.connect()
 
 
 def insert_artist():
     connection.execute("""INSERT INTO Artist(artist_name)
-    VALUES('James Blunt')
+    VALUES('Maneskin')
+    ON CONFLICT DO NOTHING""")
+    connection.execute("""INSERT INTO Artist(artist_name)
+    VALUES('Oxxxymiron')
     ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Artist(artist_name)
     VALUES('Scorpions')
-    ON CONFLICT DO NOTHING""")
-    connection.execute("""INSERT INTO Artist(artist_name)
-    VALUES('Scriptonit')
     ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Artist(artist_name)
     VALUES('Fio Rida')
@@ -34,37 +36,84 @@ def insert_artist():
 
 def insert_albums():
     connection.execute("""INSERT INTO Album
-    (album_name, artist_id, release_year)
-    VALUES('Brilliant', 6, 2015)
+    (album_name, release_year)
+    VALUES('Teatro d`ira - Vol. I', 2021)
     ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Album
-    (album_name, artist_id, release_year)
-    VALUES('Changing winds', 7, 2009)
+    (album_name, release_year)
+    VALUES('ГорГород', 2015)
     ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Album
-    (album_name, artist_id, release_year)
-    VALUES('2004', 8, 2019)
+    (album_name, release_year)
+    VALUES('Born to touch your feelings', 2017)
     ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Album
-    (album_name, artist_id, release_year)
-    VALUES('All about whistles', 9, 2018)
-    ON CONFLICT DO NOTHING""")
+        (album_name, release_year)
+        VALUES('In my mind Part 3', 2018)
+        ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Album
-    (album_name, artist_id, release_year)
-    VALUES('Beelive in Thunder', 10, 2018)
-    ON CONFLICT DO NOTHING""")
+        (album_name, release_year)
+        VALUES('Origins', 2018)
+        ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Album
-    (album_name, artist_id, release_year)
-    VALUES('In God we trust', 6, 2018)
-    ON CONFLICT DO NOTHING""")
+        (album_name, release_year)
+        VALUES('Trench', 2018)
+        ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Album
-    (album_name, artist_id, release_year)
-    VALUES('Stars', 12, 2017)
-    ON CONFLICT DO NOTHING""")
+        (album_name, release_year)
+        VALUES('Human', 2021)
+        ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Album
-    (album_name, artist_id, release_year)
-    VALUES('Perfectness', 20, 2017)
-    ON CONFLICT DO NOTHING""")
+        (album_name, release_year)
+        VALUES('=', 2018)
+        ON CONFLICT DO NOTHING""")
+
+
+def create_artists_and_albums():
+    artist = Table('Artist_', metadata,
+            Column('artist_id', Integer, autoincrement=True, primary_key=True),
+            Column('artist_name', String(40), unique=False)
+                   )
+
+    album = Table('Albums', metadata,
+           Column('album_id', Integer, autoincrement=True, primary_key=True),
+           Column('album_name', String(40), unique=False),
+           Column('release_year', Integer)
+                  )
+
+    artists_albums = Table('artists_albums', metadata,
+            Column('artist_id', Integer, ForeignKey('Artist_.artist_id'), primary_key=True),
+            Column('album_id', Integer, ForeignKey('Albums.album_id'), primary_key=True)
+    )
+    metadata.create_all(engine)
+
+    def insertion_alchemy():
+        try:
+            art_names = (['Maneskin', 'Oxxxymiron', 'Scorpions', 'Fio Rida', 'Imagine Dragons',
+                         'Twenty one pistols', 'OneRepublic', 'Ed Sheeran'])
+            alb_names = [('Teatro d`ira - Vol. I', 2021), ('ГорГород', 2015),
+                         ('Born to touch your feelings', 2017),
+                         ('In my mind Part 3', 2018), ('Origins', 2018),
+                         ('Trench', 2018), ('Human', 2021), ('=', 2018)
+                         ]
+            print(len(alb_names) == len(art_names))
+            for art_name in art_names:
+                ins = artist.insert().values(artist_name=art_name)
+                connection.execute(ins)
+            for alb_name in alb_names:
+                name, year = alb_name
+                ins_al = album.insert().values(album_name=name, release_year=year)
+                connection.execute(ins_al)
+        finally:
+            if len(alb_names) == len(art_names):
+                ar_id = 1
+                al_id = 1
+                for i in range(len(art_names)):
+                    ins = artists_albums.insert().values(artist_id=ar_id, album_id=al_id)
+                    connection.execute(ins)
+                    ar_id += 1
+                    al_id += 1
+    insertion_alchemy()
 
 
 def insert_genres():
@@ -92,95 +141,91 @@ def insert_genres():
 
 def insert_tracks():
     connection.execute("""INSERT INTO Track
-        (track_name, duration, album_id, genre_id)
-        VALUES('Organization', 173, 1, 2)
+        (track_name, duration, album_id)
+        VALUES('Begging', 211, 1)
         ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-            (track_name, duration, album_id, genre_id)
-            VALUES('My life is brilliant', 201, 5, 3)
-            ON CONFLICT DO NOTHING""")
+        (track_name, duration, album_id)
+        VALUES('ZITTI E BUONI', 211, 1)
+        ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-                (track_name, duration, album_id, genre_id)
-                VALUES('Bonfire hearts', 197, 5, 3)
-                ON CONFLICT DO NOTHING""")
+        (track_name, duration, album_id)
+        VALUES('Город под подошвой', 246, 2)
+        ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-                (track_name, duration, album_id, genre_id)
-                VALUES('Wind of Change', 311, 6, 5)
-                ON CONFLICT DO NOTHING""")
+        (track_name, duration, album_id)
+        VALUES('Переплетено', 291, 2)
+        ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-                    (track_name, duration, album_id, genre_id)
-                    VALUES('Maybe I Maybe You', 210, 6, 5)
-                    ON CONFLICT DO NOTHING""")
+        (track_name, duration, album_id)
+        VALUES('WInd of change', 311, 3)
+        ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-                    (track_name, duration, album_id, genre_id)
-                    VALUES('Кейс', 179, 7, 2)
-                    ON CONFLICT DO NOTHING""")
+        (track_name, duration, album_id)
+         VALUES('Humanity', 326, 3)
+         ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-                    (track_name, duration, album_id, genre_id)
-                    VALUES('Положение', 362, 6, 5)
-                    ON CONFLICT DO NOTHING""")
+         (track_name, duration, album_id)
+         VALUES('Whistle', 224, 4)
+         ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-                    (track_name, duration, album_id, genre_id)
-                    VALUES('Whistle', 224, 8, 6)
-                    ON CONFLICT DO NOTHING""")
+         (track_name, duration, album_id)
+         VALUES('Low', 230, 4)
+          ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-                        (track_name, duration, album_id, genre_id)
-                        VALUES('Good feeling', 241, 8, 6)
-                        ON CONFLICT DO NOTHING""")
+          (track_name, duration, album_id)
+          VALUES('Natural', 189, 5)
+          ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-                        (track_name, duration, album_id, genre_id)
-                        VALUES('Thunder', 187, 9, 4)
-                        ON CONFLICT DO NOTHING""")
+          (track_name, duration, album_id)
+          VALUES('Thunder', 187, 5)
+          ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-                            (track_name, duration, album_id, genre_id)
-                            VALUES('Natural', 189, 9, 4)
-                            ON CONFLICT DO NOTHING""")
+          (track_name, duration, album_id)
+          VALUES('Stressed Out', 202, 6)
+          ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-                            (track_name, duration, album_id, genre_id)
-                            VALUES('Heathens', 195, 10, 4)
-                            ON CONFLICT DO NOTHING""")
+          (track_name, duration, album_id)
+           VALUES('Heathens', 195, 6)
+           ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-                            (track_name, duration, album_id, genre_id)
-                            VALUES('Stressed Out', 222, 10, 7)
-                            ON CONFLICT DO NOTHING""")
+           (track_name, duration, album_id)
+           VALUES('Counting Stars', 257, 7)
+           ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-                            (track_name, duration, album_id, genre_id)
-                            VALUES('Counting Stars', 257, 11, 7)
-                            ON CONFLICT DO NOTHING""")
+           (track_name, duration, album_id)
+           VALUES('Apologize', 183, 7)
+           ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-                                (track_name, duration, album_id, genre_id)
-                                VALUES('Run', 157, 11, 7)
-                                ON CONFLICT DO NOTHING""")
+           (track_name, duration, album_id)
+           VALUES('Perfect', 263, 8)
+           ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Track
-                                (track_name, duration, album_id, genre_id)
-                                VALUES('Perfect', 263, 12, 7)
-                                ON CONFLICT DO NOTHING""")
-    connection.execute("""INSERT INTO Track
-                                (track_name, duration, album_id, genre_id)
-                                VALUES('Shape of You', 263, 12, 7)
-                                ON CONFLICT DO NOTHING""")
+           (track_name, duration, album_id)
+           VALUES('Beautiful people', 197, 8)
+           ON CONFLICT DO NOTHING""")
 
 
 def insert_mixtapes():
     connection.execute("""INSERT INTO Mixtape
         (mixtape_name, release_year)
-        VALUES(' Rap mixtape', 2017)
+        VALUES('Rock mixtape', 2017)
         ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Mixtape
             (mixtape_name, release_year)
-            VALUES('Rocking music', 2019)
+            VALUES('Pop mixtape', 2019)
             ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Mixtape
                 (mixtape_name, release_year)
-                VALUES('Sheerans mix', 2018)
+                VALUES('Dark times', 2018)
                 ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Mixtape
                 (mixtape_name, release_year)
-                VALUES('Pop mix', 2018)
+                VALUES('Relaxing', 2020)
                 ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Mixtape
                 (mixtape_name, release_year)
-                VALUES('Relax', 2010)
+                VALUES('Training', 2010)
                 ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO Mixtape
                 (mixtape_name, release_year)
@@ -199,42 +244,51 @@ def insert_mixtapes():
 def track_to_mix():
     connection.execute("""INSERT INTO TrackMixtape
                 (track_id, mixtape_id)
-                VALUES(14, 5)
+                VALUES(1, 1)
                 ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO TrackMixtape
                     (track_id, mixtape_id)
-                    VALUES(15, 10)
+                    VALUES(2, 2)
                     ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO TrackMixtape
                     (track_id, mixtape_id)
-                    VALUES(25, 13)
+                    VALUES(3, 3)
                     ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO TrackMixtape
                     (track_id, mixtape_id)
-                    VALUES(13, 14)
+                    VALUES(4, 3)
                     ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO TrackMixtape
                     (track_id, mixtape_id)
-                    VALUES(26, 15)
+                    VALUES(1, 4)
                     ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO TrackMixtape
                     (track_id, mixtape_id)
-                    VALUES(19, 16)
+                    VALUES(1, 5)
                     ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO TrackMixtape
                     (track_id, mixtape_id)
-                    VALUES(20, 17)
+                    VALUES(7, 6)
                     ON CONFLICT DO NOTHING""")
     connection.execute("""INSERT INTO TrackMixtape
                     (track_id, mixtape_id)
-                    VALUES(11, 18)
+                    VALUES(8, 8)
                     ON CONFLICT DO NOTHING""")
+
+
+def using_alchemy():
+    u = 'user_name'
+    user = Table('user', metadata,
+        Column('user_id', Integer, autoincrement=True, primary_key=True),
+        Column(u, String(30))
+                 )
+    metadata.create_all(engine)
 
 
 if __name__ == '__main__':
     insert_artist()
     insert_albums()
-    insert_genres()
     insert_tracks()
     insert_mixtapes()
     track_to_mix()
+    create_artists_and_albums()
