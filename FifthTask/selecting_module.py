967663bfdfc95ecaff3f223952ee52c656052233
OwnNightmare <yura.borisov.9523@gmail.com>
2021-11-26 10:54:41 +0300

    Tracks in 19-20

--- FifthTask/selecting_module.py
+++ FifthTask/selecting_module.py
@@ -64,95 +64,14 @@ def count_artists_in_genre():
     pprint(artists_numbers)
 
 
-def run_simple_select():
-    query = ''
-    helping = ("""
-    \t\t\t'SQL запросы:  
-    'al' - название и год выхода альбомов, вышедших в 2018 году;
-    'lt' - название и продолжительность самого длительного трека;
-    'lts' - название треков, продолжительность которых не менее 3,5 минуты;
-    'mx' - названия сборников, вышедших в период с 2018 по 2020 год включительно;
-    'art' - исполнители, чье имя состоит из 1 слова;
-    'my' - название треков, которые содержат слово "мой"/"my" ;
-    \t\t\tКоманды: 
-    'h' - вызов данной справки;
-    'q', 'exit' - выход
-    """)
-    print(helping)
-    while query not in ['q', 'exit']:
-        query = input('\tЗапрос или команда: ').lower().strip()
-        if query == 'al':
-            print('Альбомы, вышедшие в 2018:')
-            get_albums()
-        elif query == 'lt':
-            print('Самый продолжительный трек: ')
-            get_longest()
-        elif query == 'lts':
-            print('Треки не менее 3.5 минут: ')
-            get_long_tracks()
-        elif query == 'mx':
-            print('Сборники 2018 - 2020: ')
-            get_mix_in_18_20()
-        elif query == 'art':
-            print('Имена артистов из 1 слова: ')
-            get_one_worded()
-        elif query == 'my':
-            print('Трек содержит слово my/мой')
-            get_with_my()
-        elif query == 'h':
-            print('\tСправка')
-            print(helping)
-        elif query in ['q', 'exit']:
-            exit('Завершение сеанса')
-        else:
-            print('Unknown query. h - help')
-
-
-def run_complex_query():
-    query = ''
-    helping = ("""
-        \t\t\t'SQL запросы:  
-        'aq' - количество исполнителей в каждом жанре;
-        'lt' - количество треков, вошедших в альбомы 2019-2020 годов;
-        'lts' - название треков, продолжительность которых не менее 3,5 минуты;
-        'mx' - названия сборников, вышедших в период с 2018 по 2020 год включительно;
-        'art' - исполнители, чье имя состоит из 1 слова;
-        'my' - название треков, которые содержат слово "мой"/"my" ;
-        \t\t\tКоманды: 
-        'h' - вызов данной справки;
-        'q', 'exit' - выход
-        """)
-    print(helping)
-    while query not in ['q', 'exit']:
-        query = input('\tЗапрос или команда: ').lower().strip()
-        if query == 'aq':
-            print('Количество исполнителей по жанрам:')
-            count_artists_in_genre()
-        elif query == 'lt':
-            print('Самый продолжительный трек: ')
-            get_longest()
-        elif query == 'lts':
-            print('Треки не менее 3.5 минут: ')
-            get_long_tracks()
-        elif query == 'mx':
-            print('Сборники 2018 - 2020: ')
-            get_mix_in_18_20()
-        elif query == 'art':
-            print('Имена артистов из 1 слова: ')
-            get_one_worded()
-        elif query == 'my':
-            print('Трек содержит слово my/мой')
-            get_with_my()
-        elif query == 'h':
-            print('\tСправка')
-            print(helping)
-        elif query in ['q', 'exit']:
-            exit('Завершение сеанса')
-        else:
-            print('Unknown query. h - help')
+def count_tracks_in_19_20():
+    track_numbers = connection.execute("""SELECT COUNT(track_id) AS quantity 
+    FROM track t JOIN album a 
+    ON t.album_id = a.album_id
+    WHERE a.release_year BETWEEN 2019 AND 2020""").fetchall()
+    pprint(track_numbers)
 
 
 if __name__ == '__main__':
-    # run_simple_select()
     count_artists_in_genre()
 
